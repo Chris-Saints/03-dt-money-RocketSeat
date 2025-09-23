01f007a7979b0fba0e634313d948e94f76dd3391
Chris-Saints <christianpicoli18@gmail.com>
2025-09-23 12:33:23 -0300

    Criação do server.json. Para simulação de uma back-end temporaria. E a requisição das informações dentro desse back-end.

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,65 +1,64 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+	id: number;
+	description: string;
+	type: 'income' |  'outcome';
+	price: number;
+	category: string;
+	createdAt: string;
+}
+
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-            <Summary />
+	const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-            <TransactionsContainer>
-                <SearchForm />
-                
-                <TransactionsTable>
-                    <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
 
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
 
-                        <tr>
-                            <td width="50%">Hamburguer</td>
-                            <td>
-                                <PriceHighLight variant="outcome">
-                                    - R$ 5.000,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Alimentação</td>
-                            <td>13/04/2022</td>
-                        </tr>
+	async function loadTransactions() {
+		const response = await fetch('http://localhost:3000/transactions')
+		const data = await response.json();
 
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                    </tbody>
-                </TransactionsTable>
-            </TransactionsContainer>
-        </div>
-    )
+		setTransactions(data)
+		
+	}
+
+
+  useEffect(() => {
+		loadTransactions();
+	}, [])
+
+	return (
+		<div>
+			<Header />
+			<Summary />
+
+			<TransactionsContainer>
+				<SearchForm />
+				
+				<TransactionsTable>
+					<tbody>
+						{transactions.map(transaction => {
+							return(
+								<tr key={transaction.id}>
+									<td width="50%">{transaction.description}</td>
+									<td>
+										<PriceHighLight variant={transaction.type}>
+											{transaction.price}
+										</PriceHighLight>
+									</td>
+									<td>{transaction.category}</td>
+									<td>{transaction.createdAt}</td>
+								</tr>
+							)
+						})}
+
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
+		</div>
+	)
 }
